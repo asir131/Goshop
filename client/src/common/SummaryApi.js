71db8e81d82d2459e7ba365bb6fef66f37e90ce3
Asir Faysal <139361439+asir131@users.noreply.github.com>
2025-03-16 06:59:08 +0600

    upload product done

--- client/src/common/SummaryApi.js
+++ client/src/common/SummaryApi.js
@@ -79,11 +79,15 @@ const SummaryApi = {
     deleteSubCategory : {
       url : '/api/subcategory/delete',
       method : 'delete'
-  },
-  createProduct : {
-    url : '/api/product/create',
-    method : 'post'
-},
+    },
+    createProduct : {
+      url : '/api/product/create',
+      method : 'post'
+    },
+    getProduct : {
+      url : '/api/product/get',
+      method : 'post'
+    },
 
 
 }
